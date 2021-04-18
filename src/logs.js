6db6be0616cff2234106e535f75d9dc422a637e7
Mateus Koppe <mateus.koppe@gmail.com>
2021-04-18 16:45:21 -0300

    Handle checkpoints

--- src/logs.js
+++ src/logs.js
@@ -3,35 +3,83 @@ const loadLogsValues = (logs) => {
     .filter((t) => t.type == "transactionStart")
     .reduce((acc, t) => ({ ...acc, [t.data.name]: false }), {});
 
+  const getNewCheckpoint = () => ({
+    active: false,
+    changes: {},
+    actions: {},
+  });
+
+  let transactionsActions = {};
   let values = {};
-  const transactionsActions = {};
+  let checkpoint = getNewCheckpoint();
   logs.forEach((log) => {
-    console.log(log);
+    console.log({checkpoint, log})
     switch (log.type) {
       case "transactionStart":
-        transactionsActions[log.data.name] = {
-          commited: false,
-          changes: {},
-        };
+        if (checkpoint.active) {
+          checkpoint.actions[log.data.name] = {
+            commited: false,
+            changes: {},
+          };
+        } else {
+          transactionsActions[log.data.name] = {
+            commited: false,
+            changes: {},
+          };
+        }
         return;
 
       case "transactionChange":
-        transactionsActions[log.data.transaction].changes[log.data.column] =
-          log.data.value;
+        if (checkpoint.active) {
+          checkpoint.actions[log.data.transaction].changes[log.data.column] =
+            log.data.value;
+        } else {
+          transactionsActions[log.data.transaction].changes[log.data.column] =
+            log.data.value;
+        }
         return;
 
       case "transactionCommit":
-        transactionsActions[log.data.transaction].commited = true;
-        values = {
-          ...values,
-          ...transactionsActions[log.data.transaction].changes,
+        if (checkpoint.active) {
+          checkpoint.values = {
+            ...checkpoint.values,
+            ...checkpoint.actions[log.data.transaction].changes,
+          };
+          checkpoint.actions[log.data.transaction].commited = true;
+        } else {
+          transactionsActions[log.data.transaction].commited = true;
+          values = {
+            ...values,
+            ...transactionsActions[log.data.transaction].changes,
+          };
+        }
+        return;
+
+      case "checkpointStart":
+        checkpoint = {
+          active: true,
+          actions: {
+            ...transactionsActions,
+          },
+          values: { ...values },
         };
         return;
+
+      case "checkpointEnd":
+        transactionsActions = {
+          ...transactionsActions,
+          ...checkpoint.actions,
+        };
+        values = {
+          ...values,
+          ...checkpoint.values,
+        };
+        checkpoint = getNewCheckpoint();
     }
   });
 
   console.log(transactionsActions);
-  console.log(values)
+  console.log(values);
 };
 
 exports.loadLogsValues = loadLogsValues;
